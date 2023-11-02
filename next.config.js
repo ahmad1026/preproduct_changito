ff86f75c0f014135d310f69cc325b7eef0ecbe52
ahmad1026 <a.mohammadi1026@gmail.com>
2023-11-02 06:51:46 +0330

    add standalone build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        serverActions:true
-    }
+    experimental: {
+        serverActions: true
+    },
+    output: "standalone"
 }
 
 module.exports = nextConfig
